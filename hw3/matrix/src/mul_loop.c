2c40ecb4fd0ec9dd21492977c8c8564561472946
Peter <PeterRong96@gmail.com>
2020-11-10 10:46:42 -0800

    added some macro tricks
    Signed-off-by: Peter <PeterRong96@gmail.com>

--- hw3/matrix/src/mul_loop.c
+++ hw3/matrix/src/mul_loop.c
@@ -1,8 +1,15 @@
 #include "stdio.h"
 #include "stdlib.h"
+
+#ifndef OPT_LEVEL
+#define OPT_LEVEL "Opt level not specified."
+#endif
+
 int main(int argc, char** argv) {
   if (argc >= 2) {
-    fprintf(stderr, "mul_loop running with argv[0]: %s\n", argv[1]);
+    fprintf(stderr,
+            "mul_loop running with optimization level: %s, argv[0]: %s\n",
+            OPT_LEVEL, argv[1]);
   }
   return 0;
 }
